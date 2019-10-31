c280ae9066064b1f40ebd9855e11718a3a69eea4
EmilLM <luchianemil@gmail.com>
2019-11-01 01:28:29 +0200

    1/11/2019

--- src/js/ContactForm/FormBody.jsx
+++ src/js/ContactForm/FormBody.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-export default class Modal extends React.Component {
+export default class FormBody extends React.Component {
     state = {
         name: '',
         pass: '',
         mail: '',
-        option: '',
-        countries: '',
-        message: ''
+        subject: 'Select subject',
+        countries: 'Select country',
+        message: '',
+        appName: true
 
     };
     handleChange = e => {
@@ -15,64 +16,100 @@ export default class Modal extends React.Component {
             [e.target.name]: e.target.value,
             [e.target.pass]: e.target.value,
             [e.target.mail]: e.target.value,
-            [e.target.message]: e.target.value
+            [e.target.message]: e.target.value,
+            [e.target.subject]: e.target.subject,
+            [e.target.countries]: e.target.countries,
+            [e.target.appName]: false
         });
     };
     render () {
         return (
             <div className={'modal ldt-zoom-in'}>
                 <header className='modal_header ldt-slide-top-in'>
-                    <h3 className='ldt-slide-bottom-in'>Contact/Survey Form</h3>
+                    <h3 className='ldt-slide-bottom-in'>Contact</h3>
+                    <h3 className='ldt-slide-bottom-in'>Survey</h3>
                 </header>
                 <main className='ldt-fade-in'>
-                    <form className={'contactForm'}>
-                        <div className={'inputs'}>
-                            <div className="input-container">
-                                <i className="fa fa-user icon"> </i>
-                                <input autoComplete={'off'} type='text' name='name' value={this.state.name} onChange={this.handleChange} placeholder={'Name'} required/>
+                    <form className={'main_form'}>
+                        <div className={'contact_form'}>
+                            <div className={'inputs'}>
+
+                                <div className="input-container">
+                                    <i className="fa fa-user icon"> </i>
+                                    <input autoComplete={'off'} type='text' name='name' value={this.state.name} onChange={this.handleChange} placeholder={'Name'} required/>
+                                </div>
+
+                                <div className="input-container">
+                                    <i className="fa fa-envelope icon"> </i>
+                                    <input type='email' name='mail' value={this.state.mail} onChange={this.handleChange} placeholder={'Email'} />
+                                </div>
+
+                                <div className="input-container">
+                                    <i className="fa fa-unlock icon"> </i>
+                                    <input type='password' name='pass' value={this.state.pass} onChange={this.handleChange} placeholder={'Password'} required/>
+                                {/*    Need an icon for pass*/}
+                                </div>
+
                             </div>
-                            <div className="input-container">
-                                <i className="fa fa-envelope icon"> </i>
-                                <input type='email' name='mail' value={this.state.mail} onChange={this.handleChange} placeholder={'Email'} />
+                            <div className={'select_form' }>
+                                <div className={'subject_box'}>
+                                    <select id={'subject'} name='subject' value={this.state.subject} onChange={this.handleChange}  required>
+                                        <option disabled>Select subject</option>
+                                        <option value='Feedback'>Feedback</option>
+                                        <option value='Question'>Question</option>
+                                        <option value='Other'>Other</option>
+                                    </select>
+                                </div>
+                                <div className={'countries_box'}>
+                                    <select name='countries' value={this.state.countries} onChange={this.handleChange}>
+                                        <option disabled>Select country</option>
+                                        <option value="Romania">Romania</option>
+                                        <option value="USA">USA</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div className="input-container">
-                                <i className="fa fa-unlock icon"> </i>
-                                <input type='password' name='pass' value={this.state.pass} onChange={this.handleChange} placeholder={'Password'} required/>
+                            <div class={'area_box'}>
+                                <textarea name='message' value={this.state.message} onChange={this.handleChange} maxLength={'200'} placeholder={'Message'}/>
                             </div>
                         </div>
-                        {/*Tooltip about how the name sucks*/}
-                        <div className={'survey_radio'}>
-                            <p>Opinion about CountryInfo!</p>
-                            <label htmlFor="like">Like
-                                 <input type="radio" name='opinion' id='like' value='Like' checked/>
-                            </label>
-                            <label htmlFor="dislike">Dislike
-                                 <input type="radio" name='opinion' id='dislike' value='Dislike'/>
-                            </label>
+                        <div className={'survey_form'}>
+                            {/*Radio*/}
+                            <div className={'checks'}>
+                                <div className={'survey_radio'}>
+                                    <p>Opinion about CountryInfo!:</p>
+                                    <div className={'inputGroup'}>
+                                        <input type="radio" name='opinion' id='like' value='Like' checked={this.state.appName} onChange={this.handleChange}/>
+                                        <label htmlFor="like">Looks good!</label>
+                                     </div>
+                                    <div className={'inputGroup'}>
+                                        <input type="radio" name='opinion' id='dislike' value='Dislike'/>
+                                        <label htmlFor="dislike">Could be better!</label>
+                                    </div>
+                                </div>
+                                {/*Checkboxes*/}
+                                <div className={'survey_checkbox'}>
+                                    <p>What exactly?</p>
+                                    <div className={'inputGroup'}>
+                                        <input type="checkbox" id='design' />
+                                        <label htmlFor="design">Design</label>
+                                    </div>
+                                    <div className={'inputGroup'}>
+                                        <input type="checkbox" name='functionality' value='functionality' id='function'/>
+                                        <label htmlFor="function">Functionality</label>
+                                    </div>
+                                    <div className={'inputGroup tooltip'}>
+                                        <input type="checkbox" name='appName' value={this.state.appName} onChange={this.handleChange} id='appName' />
+                                        <label htmlFor="appName">Name</label>
+                                        <div className="right">
+                                            <p className='tooltipText'>
+                                                I know, it's bad. Could use some suggestions.
+                                            </p>
+                                            <i> </i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className={'survey_checkbox'}>
-                            <label htmlFor="design">Design
-                                <input type="checkbox" id='design' />
-                            </label>
-                            <label htmlFor="function">Functionality
-                                 <input type="checkbox" name='functionality' value='functionality' id='function'/>
-                            </label>
-                            <label htmlFor="appName">Name
-                                <input type="checkbox" name='appName' value='appName' id='appName' checked/>
-                            </label>
-                        </div>
-                        <div className={'select_form'}>
-                            <select value={this.state.option}>Subject
-                                <option value='Feedback'>Feedback</option>
-                                <option value='Question'>Question</option>
-                                <option value='Other'>Other</option>
-                            </select>
-                            <select value={this.state.countries}>
-                                <option value="Romania">Romania</option>
-                                <option value="USA">USA</option>
-                            </select>
-                        </div>
-                        <textarea name='message' value={this.state.message} onChange={this.handleChange} maxLength={'200'} placeholder={'Message'}/>
                         <button type={'submit'} className={'btn'}>Submit</button>
 
                         {/*Pictures in input + check icon as validation*/}
