89e19440c77548b5697f910bcb11e67e121911bc
EmilLM <luchianemil@gmail.com>
2019-11-04 00:19:26 +0200

    4/11/2019

--- src/js/ContactForm/AfterSubmit.jsx
+++ src/js/ContactForm/AfterSubmit.jsx
@@ -5,8 +5,8 @@ export default class AfterSubmit extends React.Component {
         return (
             <main className='ldt-power-on'>
                 <h3>Thank you for your {this.props.selectInput}, {this.props.nameInput}!</h3>
-                {this.props.radioInput === 'like' && <p>I knew it! It does look good :D</p>}
-                {this.props.radioInput === 'dislike' && <p>Guess I'll keep working on it. :(</p>}
+                {this.props.radioInput === 'like' && <p style={{textAlign: 'center'}}>I knew it! It does look good :D</p>}
+                {this.props.radioInput === 'dislike' && <p style={{textAlign: 'center'}}>Guess I'll keep working on it. :(</p>}
             </main>
         )
     }
